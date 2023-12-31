d9e206648b09a15ba34bfc547c6b0ce7ebae8d5c
Neo H <neo.hong@tu-dortmund.de>
2023-12-31 15:35:30 +0100

    Edited script.r to display the data in lieblinge.csv

--- script.r
+++ script.r
@@ -1,10 +1,25 @@
-library(dplyr, exclude = c("filter", "lag"))
 library(tidyverse)
 
-favorites <- read.csv("lieblinge.csv",TRUE,",")
+favorites <- read.csv("lieblinge.csv", TRUE, ",")
 
-ggplot(favorites, aes(Lieblingsfarbe, Alter))+
-    coord_flip()+
-    stat_boxplot(geom='errorbar', width=0.8)+
-    geom_boxplot()+
-    labs(x="Alter", y="Lieblingsfarbe")
+library(forcats)
+favorites$Lieblingstier <- fct_other(favorites$Lieblingstier, keep = c("Hund", "Katze"), other_level = "Andere")
+
+ggplot(favorites, aes(Lieblingsfarbe, Alter)) +
+    stat_boxplot(geom = "errorbar", width = 0.75) +
+    coord_flip() +
+    geom_boxplot() +
+    labs(x = "Lieblingsfarbe", y = "Alter")
+
+ggplot(favorites, aes(Lieblingstier, Alter)) +
+    stat_boxplot(geom = "errorbar", width = 0.75) +
+    coord_flip() +
+    geom_boxplot() +
+    labs(x = "Lieblingstier", y = "Alter")
+
+library(ggmosaic)
+
+ggplot(favorites) +
+    geom_mosaic(aes(product(Lieblingsfarbe, Lieblingstier), fill = Lieblingsfarbe)) +
+    scale_fill_manual(values = c("lightblue3", "khaki", "palegreen3", "palevioletred")) +
+    labs(x = "Lieblingstier", y = "Lieblingsfarbe")
